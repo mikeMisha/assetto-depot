1ccf9451e7e5c6bd27bb7980e4d964e641b88644
mikeMisha <62260889+mikeMisha@users.noreply.github.com>
2022-09-23 16:04:05 -0400

    remote domains from config file

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   reactStrictMode: true,
   images: {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    domains: ['usercontent.one', 'assettocorsa.club', 'res.cloudinary.com'],
+    domains: ['res.cloudinary.com'],
   },
   experimental: {
     scrollRestoration: true,
