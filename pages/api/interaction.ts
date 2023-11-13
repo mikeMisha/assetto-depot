70b7388b4b26bc9f4fce7f64ddda4e62723edfa6
Michael Karabach <62260889+mikeMisha@users.noreply.github.com>
2023-11-13 12:58:48 -0700

    implemented liked and disliked feature

--- pages/api/interaction.ts
+++ pages/api/interaction.ts
@@ -1,18 +1,70 @@
 import { supabase } from '../../src/lib/initSupabase';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
+type ReactionRequestBody = {
+  dataCategory: string;
+  id: string;
+  reaction: 'like' | 'dislike' | null; // Updated to include null
+  previousReaction: 'like' | 'dislike' | null;
+};
+
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { liked, disliked, dataCategory, id } = req.body;
-  console.log('req.body', req.body);
   if (req.method === 'POST') {
-    const { data, error } = await supabase
-      .from(dataCategory)
-      .update({ liked, disliked })
-      .eq('id', id);
-    console.error(error);
-    console.error('data', data);
+    const { dataCategory, id, reaction, previousReaction } =
+      req.body as ReactionRequestBody;
+    console.log('req.body', req.body);
+
+    try {
+      // Fetch current likes and dislikes
+      const { data: currentData, error: fetchError } = await supabase
+        .from(dataCategory)
+        .select('likes, dislikes')
+        .eq('id', id)
+        .single();
+
+      if (fetchError) throw fetchError;
+
+      let { likes, dislikes } = currentData;
+
+      // Adjust likes and dislikes based on the reaction and previousReaction
+      if (reaction === 'like') {
+        likes++;
+        if (previousReaction === 'dislike') {
+          dislikes = Math.max(dislikes - 1, 0);
+        }
+      } else if (reaction === 'dislike') {
+        dislikes++;
+        if (previousReaction === 'like') {
+          likes = Math.max(likes - 1, 0);
+        }
+      } else if (reaction === null) {
+        // User retracts their previous reaction
+        if (previousReaction === 'like') {
+          likes = Math.max(likes - 1, 0);
+        } else if (previousReaction === 'dislike') {
+          dislikes = Math.max(dislikes - 1, 0);
+        }
+      }
+
+      // Update the likes and dislikes in the database
+      const { error: updateError } = await supabase
+        .from(dataCategory)
+        .update({ likes, dislikes })
+        .eq('id', id);
+
+      if (updateError) throw updateError;
+
+      return res
+        .status(200)
+        .json({ message: 'Interaction updated', likes, dislikes });
+    } catch (error) {
+      console.error('Error updating interaction:', error);
+      return res.status(500).json({ message: 'Error updating interaction' });
+    }
+  } else {
+    res.setHeader('Allow', ['POST']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
   }
-  res.status(200).json({ message: 'Interaction updated' });
 }
 
 export default handler;
--- src/components/LikeDislike.tsx
+++ src/components/LikeDislike.tsx
@@ -1,163 +1,50 @@
-import { useState, useEffect, useRef } from 'react';
+import React from 'react';
 import { Typography, IconButton } from '@mui/material';
 import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 import ThumbDownIcon from '@mui/icons-material/ThumbDown';
 import ThumbUpOutlinedIcon from '@mui/icons-material/ThumbUpOutlined';
 import ThumbDownOutlinedIcon from '@mui/icons-material/ThumbDownOutlined';
-import axios from 'axios';
-import { debounce, set } from 'lodash';
-import type { dataCategory } from '../types/global';
+import useLikeDislike, { LikeDislikeProps } from '../hooks/useLikeDislike'; // Adjust the import path as necessary
+import type { DataCategory } from '../types/global';
+
+interface LikeDislikeComponentProps {
+  data: LikeDislikeProps;
+  dataCategory: DataCategory;
+}
+
 const thumbStyle = {
   mr: 0.5,
   '&:hover': { color: 'primary.main', cursor: 'pointer' },
 };
 
-interface LikeDislikeProps {
-  data: {
-    id: string;
-    likes: number;
-    dislikes: number;
-  };
-  dataCategory: dataCategory;
-}
-
-interface InteractionItem {
-  id: string;
-  liked: boolean;
-  disliked: boolean;
-  wasActive: boolean;
-}
-
-interface StoredInteractions {
-  [key: string]: InteractionItem[];
-}
-
-const LikeDislike = ({ data, dataCategory }: LikeDislikeProps) => {
-  const [likes, setLikes] = useState(data.likes);
-  const [dislikes, setDislikes] = useState(data.dislikes);
-  const [likeActive, setLikeActive] = useState(false);
-  const [dislikeActive, setDislikeActive] = useState(false);
-
-  // Migrate the local storage update to a separate function
-  const updateLocalStorage = (interaction: InteractionItem) => {
-    const localInteractionsStr = localStorage.getItem('interactions');
-    const localInteractions: StoredInteractions = localInteractionsStr
-      ? JSON.parse(localInteractionsStr)
-      : {};
-
-    const updatedItems =
-      localInteractions[dataCategory]?.filter((i) => i.id !== interaction.id) ||
-      [];
-    updatedItems.push(interaction);
-
-    localStorage.setItem(
-      'interactions',
-      JSON.stringify({ ...localInteractions, [dataCategory]: updatedItems })
-    );
-
-    const payload = {
-      dataCategory: dataCategory,
-      id: interaction.id,
-      liked: interaction.liked,
-      disliked: interaction.disliked,
-      wasActive: interaction.wasActive,
-    };
-    axios.post('/api/interaction', payload).catch((err) => {
-      console.error(
-        err.response?.status === 404
-          ? 'Resource could not be found!'
-          : err.message
-      );
-    });
-  };
-
-  // Debounce the local storage update function
-  const debouncedUpdateLocalStorage = debounce(updateLocalStorage, 300);
-
-  // Effect for initializing the like/dislike active states from local storage
-  useEffect(() => {
-    const localInteractionsStr = localStorage.getItem('interactions');
-    const localInteractions: StoredInteractions = localInteractionsStr
-      ? JSON.parse(localInteractionsStr)
-      : {};
-
-    const interaction = localInteractions[dataCategory]?.find(
-      (i) => i.id === data.id
-    );
-    if (interaction) {
-      setLikeActive(interaction.liked);
-      setDislikeActive(interaction.disliked);
-    }
-  }, [data.id, dataCategory]);
-
-  const onThumbClick = (thumbType: 'up' | 'down') => {
-    // If the thumb is already active, reset the state
-    const wasActive = likeActive || dislikeActive;
-    if (thumbType === 'up') {
-      if (likeActive) {
-        setLikes((prev) => (prev > 0 ? prev - 1 : 0)); // Decrement likes only if it was previously liked
-        setLikeActive(false); // Reset like active state
-      } else {
-        setLikes((prev) => prev + 1); // Increment likes
-        setLikeActive(true);
-        if (dislikeActive) {
-          setDislikes((prev) => prev - 1); // Decrement dislikes if it was previously disliked
-          setDislikeActive(false); // Reset dislike active state
-        }
-      }
-    } else if (thumbType === 'down') {
-      if (dislikeActive) {
-        setDislikes((prev) => (prev > 0 ? prev - 1 : 0));
-        setDislikeActive(false); // Reset dislike active state
-      } else {
-        setDislikes((prev) => prev + 1);
-        setDislikeActive(true);
-        if (likeActive) {
-          setLikes((prev) => prev - 1); // Decrement likes if it was previously liked
-          setLikeActive(false); // Reset like active state
-        }
-      }
-    }
-
-    debouncedUpdateLocalStorage({
-      id: data.id,
-      liked: thumbType === 'up',
-      disliked: thumbType === 'down',
-      wasActive,
-    });
-  };
-
-  // Simplify the thumb rendering function
-  const renderThumb = (
-    thumbType: 'up' | 'down',
-    isActive: boolean,
-    IconFilled: typeof ThumbUpIcon | typeof ThumbDownIcon,
-    IconOutlined: typeof ThumbUpOutlinedIcon | typeof ThumbDownOutlinedIcon
-  ) => {
-    const onClick = () => {
-      onThumbClick(thumbType);
-    };
-
-    return (
-      <IconButton onClick={onClick} sx={thumbStyle}>
-        {isActive ? <IconFilled color="primary" /> : <IconOutlined />}
-      </IconButton>
-    );
-  };
+const LikeDislike = (props: LikeDislikeComponentProps) => {
+  const { data, dataCategory } = props;
+  const { likes, dislikes, likeActive, dislikeActive, handleInteraction } =
+    useLikeDislike(data, dataCategory);
 
   return (
     <>
       <Typography sx={{ mr: 2 }}>
-        {renderThumb('up', likeActive, ThumbUpIcon, ThumbUpOutlinedIcon)}
+        <IconButton onClick={() => handleInteraction('like')} sx={thumbStyle}>
+          {likeActive ? (
+            <ThumbUpIcon color="primary" />
+          ) : (
+            <ThumbUpOutlinedIcon />
+          )}
+        </IconButton>
         {likes}
       </Typography>
       <Typography sx={{ mr: 2 }}>
-        {renderThumb(
-          'down',
-          dislikeActive,
-          ThumbDownIcon,
-          ThumbDownOutlinedIcon
-        )}
+        <IconButton
+          onClick={() => handleInteraction('dislike')}
+          sx={thumbStyle}
+        >
+          {dislikeActive ? (
+            <ThumbDownIcon color="primary" />
+          ) : (
+            <ThumbDownOutlinedIcon />
+          )}
+        </IconButton>
         {dislikes}
       </Typography>
     </>
