9f0453ed5df1bd445ce034a98699281626be541e
Michael Karabach <62260889+mikeMisha@users.noreply.github.com>
2023-11-12 21:39:55 -0700

    small edit to supabase call for interactions

--- pages/api/interaction.ts
+++ pages/api/interaction.ts
@@ -2,12 +2,12 @@ import { supabase } from '../../src/lib/initSupabase';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { liked, disliked, dataCategory, id, wasActive } = req.body;
+  const { liked, disliked, dataCategory, id } = req.body;
   console.log('req.body', req.body);
   if (req.method === 'POST') {
     const { data, error } = await supabase
       .from(dataCategory)
-      .update({})
+      .update({ liked, disliked })
       .eq('id', id);
     console.error(error);
     console.error('data', data);
